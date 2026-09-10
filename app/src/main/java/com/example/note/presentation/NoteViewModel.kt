c446b2acaf87b2356f9f3043b52528c0a5e86bdc
garatlakov <garatlakov@abv.bg>
2026-09-11 00:28:16 +0300

    Added viewModel.

---
+++ app/src/main/java/com/example/note/presentation/NoteViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.note.presentation
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.note.domain.model.NoteUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class NoteViewModel@Inject constructor
+    (private val noteUseCases: NoteUseCases): ViewModel() {
+
+        private val _state = mutableStateOf(NotesState())
+        val state : State<NotesState> = _state
+
+        init {
+            getNotes()
+        }
+
+        fun onEvent(event: NoteEvents){
+            when(event){
+                is NoteEvents.DeleteNote -> {
+                    viewModelScope.launch {
+                        noteUseCases.deleteNote(event.note)
+                    }
+                }
+                is NoteEvents.UpsertNote -> {
+                    viewModelScope.launch {
+                        noteUseCases.upsertNotes(event.note)
+                    }
+                }
+            }
+        }
+
+    private fun getNotes(){
+        viewModelScope.launch {
+            noteUseCases.getNotes().collect{
+                _state.value = state.value.copy(
+                    notes = it
+                )
+            }
+        }
+    }
+}
