b12a7c0c04c4984230b4a8fe3ab2c0435dfa38c5
garatlakov <garatlakov@abv.bg>
2026-09-11 00:03:20 +0300

    Added note events.

---
+++ app/src/main/java/com/example/note/presentation/NoteEvents.kt
@@ -0,0 +1,8 @@
+package com.example.note.presentation
+
+import com.example.note.domain.model.Note
+
+sealed class NoteEvents {
+    data class DeleteNote(val note: Note): NoteEvents()
+    data class UpsertNote(val note: Note): NoteEvents()
+}
