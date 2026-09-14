48d3bc3b3c5be3f998f1ae9e236852486c495472
garatlakov <garatlakov@abv.bg>
2026-09-14 22:12:00 +0300

    Added top bar compose.

---
+++ app/src/main/java/com/example/note/presentation/components/TopAppBar.kt
@@ -0,0 +1,43 @@
+@file:Suppress("PreviewAnnotationInFunctionWithParameters")
+
+package com.example.note.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.note.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(onNavigationIconClick:  () -> Unit){
+    TopAppBar(
+        title = {
+        },
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.onTertiary),
+        navigationIcon = {
+            IconButton(
+                onClick = onNavigationIconClick
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.back64),
+                    contentDescription = "Back"
+                )
+            }
+        },
+
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun TopAppBarPreview(onNavigationIconClick:  () -> Unit){
+    TopAppBar(onNavigationIconClick = onNavigationIconClick)
+}
