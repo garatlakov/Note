f30160778e6280221f7c9cca599423e306c1ba8b
garatlakov <garatlakov@abv.bg>
2026-09-24 09:47:06 +0300

    UpsertNoteScreen is complete.

--- app/src/main/java/com/example/note/presentation/screen/UpsertNoteScreen.kt
+++ app/src/main/java/com/example/note/presentation/screen/UpsertNoteScreen.kt
@@ -1,40 +1,59 @@
 package com.example.note.presentation.screen
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.note.domain.model.Note
+import com.example.note.presentation.components.NoteTopAppBar
 
 @Composable
-fun UpsertNoteScreen(){
-
+fun UpsertNoteScreen(
+    note: Note,
+    onNavigationIconClick:  () -> Unit,
+    onSaveNoteClick: () -> Unit
+){
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { NoteTopAppBar(
+            onSaveNoteClick = onSaveNoteClick,
+            onNavigationIconClick = onNavigationIconClick
+        ) }
+    ) {
+            innerPadding -> UpsertNoteContext(
+                modifier = Modifier.padding(innerPadding),
+                note = note
+            )
+    }
 }
 
 @Composable
 fun UpsertNoteContext(modifier: Modifier = Modifier, note: Note){
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxWidth()
     ) {
         OutlinedTextField(
             value = note.name,
             onValueChange = { },
-            modifier = modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
             label = { }
         )
-        Spacer(
-            modifier = modifier.size(10.dp)
-        )
+
         OutlinedTextField(
             value = note.description,
             onValueChange = { },
-            modifier = modifier.fillMaxHeight(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(5.dp),
             label = { }
         )
     }
@@ -43,12 +62,13 @@ fun UpsertNoteContext(modifier: Modifier = Modifier, note: Note){
 @Composable
 @Preview(showBackground = true)
 fun UpsertNoteScreenPreview() {
-    UpsertNoteContext(
-        modifier = Modifier,
+    UpsertNoteScreen(
         note = Note(
             name = "Тест",
             description = "Това е тест, най готиния тест в света на тестовете.",
             id = 1
-        )
+        ),
+        onSaveNoteClick = {},
+        onNavigationIconClick = {}
     )
 }
